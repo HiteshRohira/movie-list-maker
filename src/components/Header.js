63183a4f2efe2e402e610a797a28de44e8695dd6
HiteshRohira <hiteshrohira15@gmail.com>
2021-02-01 15:20:08 +0530

    Added hamburger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,28 +3,43 @@ import { Link } from "react-router-dom";
 import logo from "../img/logo.svg";
 
 const Header = () => {
+	const openMenu = () => {
+		const navbar = document.querySelector("#nav");
+		navbar.classList.toggle("show");
+	};
+
+	const closeMenu = () => {
+		const navbar = document.querySelector("#nav");
+		navbar.classList.remove("show");
+	};
+
 	return (
 		<header>
 			<span className="title">
 				<img src={logo} alt="Logo" id="logo" />
-				<h2>Movie List Maker</h2>
+				<h2>Movie Lister</h2>
 			</span>
-			<ul className="nav-links">
-				<li>
-					<Link to="/">Popular</Link>
-				</li>
-				<li>
-					<Link to="/top-rated">Top Rated</Link>
-				</li>
-				<li>
-					<Link to="/user-list">My List</Link>
-				</li>
-				<li>
-					<Link to="/search">
-						<i className="fas fa-search"></i>
-					</Link>
-				</li>
-			</ul>
+			<button className="menu-btn" onClick={openMenu}>
+				<i className="fas fa-bars menu"></i>
+			</button>
+			<nav id="nav">
+				<ul className="nav-links">
+					<li onClick={closeMenu}>
+						<Link to="/">Popular</Link>
+					</li>
+					<li onClick={closeMenu}>
+						<Link to="/top-rated">Top Rated</Link>
+					</li>
+					<li onClick={closeMenu}>
+						<Link to="/user-list">My List</Link>
+					</li>
+					<li onClick={closeMenu}>
+						<Link to="/search">
+							<i className="fas fa-search"></i>
+						</Link>
+					</li>
+				</ul>
+			</nav>
 		</header>
 	);
 };
